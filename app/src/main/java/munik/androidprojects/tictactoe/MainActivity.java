e727004514ca55bb1f7c60d504a537c860dcc393
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-04-07 00:13:59 +0530

    added boolean endgame so that if a player wins the game stops there!

--- app/src/main/java/munik/androidprojects/tictactoe/MainActivity.java
+++ app/src/main/java/munik/androidprojects/tictactoe/MainActivity.java
@@ -12,32 +12,36 @@ public class MainActivity extends AppCompatActivity {
     public int turn = 0;
     public int[] gridFilled = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
     int[][] winCases = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}};
+    Boolean Endgame = false;
 
     public void click(View view) {
 
 
         ImageView gridClicked = (ImageView) view;
-        if (gridFilled[Integer.parseInt(gridClicked.getTag().toString()) - 1] == -1) {
-            //define players turn and prind desired sign in desired grid of the bord
-            if (turn == 0) {
-                gridClicked.setImageResource(R.drawable.cirle);
-                gridFilled[Integer.parseInt(gridClicked.getTag().toString()) - 1] = turn;
-                turn = 1;
-            } else {
-                gridClicked.setImageResource(R.drawable.cross);
-                gridFilled[Integer.parseInt(gridClicked.getTag().toString()) - 1] = turn;
-                turn = 0;
-            }
-            //to check win case!!
-            for (int[] onewinCase : winCases) {
-                if (gridFilled[onewinCase[0] - 1] == gridFilled[onewinCase[1] - 1] && gridFilled[onewinCase[1] - 1] == gridFilled[onewinCase[2] - 1] && gridFilled[onewinCase[0] - 1] != -1) {
-                    if (turn == 0) {
-                        Toast.makeText(this, "crossWON!!", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(this, "crisWON!!", Toast.LENGTH_SHORT).show();
+        if (Endgame == true) {
+            if (gridFilled[Integer.parseInt(gridClicked.getTag().toString()) - 1] == -1) {
+                //define players turn and prind desired sign in desired grid of the bord
+                if (turn == 0) {
+                    gridClicked.setImageResource(R.drawable.cirle);
+                    gridFilled[Integer.parseInt(gridClicked.getTag().toString()) - 1] = turn;
+                    turn = 1;
+                } else {
+                    gridClicked.setImageResource(R.drawable.cross);
+                    gridFilled[Integer.parseInt(gridClicked.getTag().toString()) - 1] = turn;
+                    turn = 0;
+                }
+                //to check win case!!
+                for (int[] onewinCase : winCases) {
+                    if (gridFilled[onewinCase[0] - 1] == gridFilled[onewinCase[1] - 1] && gridFilled[onewinCase[1] - 1] == gridFilled[onewinCase[2] - 1] && gridFilled[onewinCase[0] - 1] != -1) {
+                        Endgame = true;
+                        if (turn == 0) {
+                            Toast.makeText(this, "crossWON!!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(this, "crisWON!!", Toast.LENGTH_SHORT).show();
+
+                        }
 
                     }
-
                 }
             }
         }
