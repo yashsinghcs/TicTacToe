8837672bc9f6b1f85e9c37439963529518c53a8c
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-04-06 19:52:52 +0530

    added turnwise criss cross print on  the board without any winn case implemented

--- app/src/main/java/munik/androidprojects/tictactoe/MainActivity.java
+++ app/src/main/java/munik/androidprojects/tictactoe/MainActivity.java
@@ -10,8 +10,19 @@ public class MainActivity extends AppCompatActivity {
     public void click(View view) {
         Boolean[] gridFilled = {false, false, false, false, false, false, false, false, false};
         int[][] winCases = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}};
-        ImageView gridClicked = (ImageView) view;
-        gridClicked.setImageResource(R.drawable.cirle);
+        //turn =0:player 1 i.e  cross & if turn=1: player2  i.e criss
+        int turn = 0;
+        for (int freeSpaces = 0; freeSpaces < 9; freeSpaces++) {
+            if (turn == 0) {
+                ImageView gridClicked = (ImageView) view;
+                gridClicked.setImageResource(R.drawable.cirle);
+                turn = 1;
+            } else {
+                ImageView gridClicked = (ImageView) view;
+                gridClicked.setImageResource(R.drawable.cross);
+                turn = 0;
+            }
+        }
 
     }
 
