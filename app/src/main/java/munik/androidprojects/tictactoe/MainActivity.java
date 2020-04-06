e8e7841a36dd5b25e07b62f7e0ffb3ad2a8c11cc
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-04-06 19:01:51 +0530

    create a click class in java that has the argument of View type that will help player to click on desired grid to mark their token

--- app/src/main/java/munik/androidprojects/tictactoe/MainActivity.java
+++ app/src/main/java/munik/androidprojects/tictactoe/MainActivity.java
@@ -1,10 +1,14 @@
 package munik.androidprojects.tictactoe;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public void click(View view) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
