2c68052b7d5fefe36b2a42941bd35a0d8be15e39
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-04-06 19:42:29 +0530

    added boolen arry variable gridFilled to take record of the grids filled and a int 2d array to store all winning possiblities

--- app/src/main/java/munik/androidprojects/tictactoe/MainActivity.java
+++ app/src/main/java/munik/androidprojects/tictactoe/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     public void click(View view) {
+        Boolean[] gridFilled = {false, false, false, false, false, false, false, false, false};
+        int[][] winCases = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}};
         ImageView gridClicked = (ImageView) view;
         gridClicked.setImageResource(R.drawable.cirle);
 
